a252dc12b8e9915fba4c4707205e4a7bcb2bc5f1
SevenNorth <qibeidu@163.com>
2025-03-23 15:29:10 +0800

    fix: title

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: '健康管家',
+  title: '津医卫',
   description: '',
   copyright: '',
   privacy_policy: '',
