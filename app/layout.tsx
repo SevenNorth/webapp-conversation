b2f3266a04c1be27e92d5dc7819c39e1f0ba0780
SevenNorth <qibeidu@163.com>
2025-03-27 07:00:57 +0800

    feat: 尝试禁用缩放

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ const LocaleLayout = ({
   const locale = getLocaleOnServer()
   return (
     <html lang={locale ?? 'en'} className="h-full">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
       <body className="h-full">
         <div className="overflow-x-auto">
           <div className="w-screen h-screen min-w-[300px]">
