86e3ac4de96da6a5dc0a78e2e1891bb87e8735a5
SevenNorth <qibeidu@163.com>
2025-04-14 16:20:24 +0800

    feat: longquan

--- config/index.ts
+++ config/index.ts
@@ -21,9 +21,9 @@ export const DEFAULT_VALUE_MAX_LEN = 48
 
 // 显示模式切换
 export const APP_SHOW_MODE_SWITCH = true
-export const APP_INPUT_PARAMS_MODE_KEY = 'ms'
+export const APP_INPUT_PARAMS_MODE_KEY = 'moshi'
 export const APP_MODE_OPTIONS: IModeOpts[] = [
   { label: '健康咨询', value: 'consultation', pushBotMsg: true, botMsg: 'AAA' },
-  { label: '智能导诊', value: 'guide', pushBotMsg: true, botMsg: 'BBB' },
+  { label: '智能导诊', value: '智能导诊', pushBotMsg: true, botMsg: 'BBB' },
   { label: '就诊小结', value: 'summary', pushBotMsg: false, botMsg: 'summary', pushQuery: true, queryMsg: '查询就诊小结' },
 ]
